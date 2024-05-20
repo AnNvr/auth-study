c8186823f9a9e980339be4472eb3bb6a37b810c1
andrea.nvr <an.nvr@gmail.com>
2024-05-20 21:23:17 +1000

    Update credentials.js

--- backend/middleware/credentials.js
+++ backend/middleware/credentials.js
@@ -1,8 +1,14 @@
 import { whiteList } from "../config/corsOptions.js";
 
 export const credentials = (req, res, next) => {
+    // extract the `Origin` header from the incoming request
     const origin = req.headers.origin
+
+    // check if the origin is included in the whitelist
     if (whiteList.includes(origin)) {
+
+        // crucial for authentication: allows credential to be included on requests
+        // made to this domain
         res.header('Access-Control-Allow-Credentials', true)
     }
     next()
